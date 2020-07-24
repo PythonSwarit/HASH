4a10eb50fc6fa0b332ad739701266324fe1f9645
Swarit Choudhari <68472469+PythonSwarit@users.noreply.github.com>
2020-07-24 11:34:52 -0400

    Create Stuff.js

---
+++ Stuff.js
@@ -0,0 +1,13 @@
+{
+  "name": "hash",
+  "version": "1.0.0",
+  "lockfileVersion": 1,
+  "requires": true,
+  "dependencies": {
+    "sha2": {
+      "version": "1.0.2",
+      "resolved": "https://registry.npmjs.org/sha2/-/sha2-1.0.2.tgz",
+      "integrity": "sha512-sQ1sFn+WN2weGkwwt25bllwocaJLyYeDrn/T221Bovc6XEHRf7NSn0KPoPQMV2+NT+RhJoStkdm4TrEGCXkKHA=="
+    }
+  }
+}
